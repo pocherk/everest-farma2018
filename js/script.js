f023fe868961932717d48b8f9727bc307f3c5443
Pocherk <pocherk@under-constraction.ru>
2018-06-20 17:16:37 +0300

    zajivlenie and boston-uro on one page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+
+    var f = $('.first');
+        s = $('.second');
+        b = $('.mb-30')
+        s.css({'background':'#ffffff'});
+        b.css({'color':'#000000'})
+
+
+
+
+});
